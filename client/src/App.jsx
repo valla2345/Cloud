6f446c7783165f9ae92e3633afcd345460e95f83
valla2345 <yui2345@naver.com>
2026-02-11 16:30:48 +0900

    proxt edit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,12 @@ function App() {
   const [data, setData] = useState('')
   const [obj, setObj] = useState({})
 
-  const sendToServer = ()=>{
+  const sendToServer = () => {
     console.log('sendToServer', data)
 
     // front 데이터를 server로 전송 
-    axios.post('http://localhost:3000/getData', {
-      data : data
+    axios.post('http://myweb/getData', {
+      data: data
     }).then(res => {
       console.log('res', res.data)
       setObj(res.data)
@@ -21,12 +21,12 @@ function App() {
     <>
       <h1>
         <p>Client에서 Server로 데이터 전송하기</p>
-        <input type="text" onChange={(e)=>{setData(e.target.value)}}></input>
+        <input type="text" onChange={(e) => { setData(e.target.value) }}></input>
         <button onClick={sendToServer}>전송</button>
-        <hr/>
+        <hr />
         <p>Server에서 Client로 넘어온 데이터</p>
         {obj.status == 200 &&
-         <p>{obj.nick} 님 환영합니다.</p>
+          <p>{obj.nick} 님 환영합니다.</p>
         }
       </h1>
     </>
